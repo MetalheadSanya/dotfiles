ab95f781719ea6bc0551d9c5c2fd021b589047b8
a.zalutskiy <a.zalutskiy@tinkoff.ru>
2025-05-13 12:06:31 +0500

    Initial commit

---
+++ bin/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e; \
+    ARCH_NAME="$(uname -m)"; \
+    url=; \
+    case "${ARCH_NAME}" in \
+        'amd64') \
+            url='https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz'; \
+            ;; \
+        'arm64') \
+            url='https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz'; \
+            ;; \
+        *) echo >&2 "error: unknown architecture: ${ARCH_NAME}"; exit 1 ;; \
+    esac; \
+    curl -kfsSL "${url}" -o nvim.tar.gz; \
+    mkdir -p /opt/nvim; \
+    tar -xzf nvim.tar.gz --directory /opt/nvim --strip-components 1; \
+    rm nvim.tar.gz;
+
+rsync -a config/ ~/.config/
